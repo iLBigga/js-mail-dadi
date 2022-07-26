68baf6506a1e881c5bd58039cc0dff5b86a543f2
iLBigga <Giannanti1996@gmail.com>
2022-07-26 10:35:19 +0200

    Refactory

--- Esercizio Dadi/js/app.js
+++ Esercizio Dadi/js/app.js
@@ -3,31 +3,39 @@ const player = [];
 // Creo array bot
 const bot = []
 
-// Genero Numero random per player
-const playerRandom =  Math.floor(Math.random() * 6 + 1 );
-// Pusho il risultato in player
-player.push(playerRandom)
 
-// Genero Numero random per bot
-const botRandom =  Math.floor(Math.random() * 6 + 1 );
-// Pusho il risultato in bot
-bot.push(botRandom)
+const playerData = document.getElementById('player')
+const botData = document.getElementById('bot')
+const resultData = document.getElementById('result')
 
-// Creo variabile per identificare elemento 0 nell'array
-const playerResult = player[0]
-const botResult = bot[0]
+const submitElement = document.querySelector('input[name="submit"]')
 
-// Confronto risultato numero random
-// SE playerResult è maggiore di botResult
-if (playerResult > botResult) {
-    // Stampo 'Player vince'
-    console.log('Player vince')
-// ALTRIMENTI SE il risultato è uguale
-} else if (playerResult === botResult){
-     // Stampo 'Pareggio'
-    console.log('Pareggio')
-// ALTRIMENTI
-} else {
-    // Stampo 'Bot vince'
-    console.log('Bot vince')
-}
+
+submitElement.addEventListener('click', function () {
+
+    // Genero Numero random per player
+    const playerRandom =  Math.floor(Math.random() * 6 + 1 );
+    playerData.innerHTML = 'Player dado ' + playerRandom
+
+    // Genero Numero random per bot
+    const botRandom =  Math.floor(Math.random() * 6 + 1 );
+    botData.innerHTML = 'Bot dado ' + botRandom
+
+    let messaggio = 'Pareggio'
+
+    // Confronto risultato numero random
+    // SE playerResult è maggiore di botResult
+    if (playerRandom > botRandom) {
+        // Stampo 'Player vince'
+        messaggio = 'Player vince'
+        resultData.innerHTML = messaggio
+    // ALTRIMENTI SE il risultato è uguale
+    } else if (playerRandom < botRandom){
+        // Stampo 'Bot vince'
+        messaggio = 'Bot vince'
+        resultData.innerHTML = messaggio
+    } 
+
+    console.log(messaggio)
+
+})
